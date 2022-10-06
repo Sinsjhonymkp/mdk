364248a80bc55be78821cc6e69031a81ff348705
Sinsjhonymkp <tohaantoha96@gmail.com>
2022-10-06 11:45:36 +0300

    first commit

---
+++ log/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace log
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserValidator.Validate(tbLogin.Text, tbPassword.Text))
+            {
+                new user().Show();
+                Close();
+            }
+            else MessageBox.Show("Invalid log or password","Warning",MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
